d8addf5342c15f6fd8dab6f8d9fd94933d503a56
Guilherme Cardoso <gui.cardoso.28@gmail.com>
2024-05-24 18:31:29 -0300

    Criação das props do componente TicketCard e seus subcomponentes para rederização dinamica dos tickets na tela

--- app/(components)/StatusDisplay.jsx
+++ app/(components)/StatusDisplay.jsx
@@ -1,9 +1,33 @@
 import React from "react";
 
-const StatusDisplay = () => {
+const StatusDisplay = ({ status }) => {
+  const getBgColor = (status) => {
+    let color = "bg-slate-700";
+
+    switch (status.toLowerCase()) {
+      case "terminado":
+        color = "bg-green-200";
+        return color;
+
+      case "iniciado":
+        color = "bg-yellow-200";
+        return color;
+
+      case "não iniciado":
+        color = "bg-red-200";
+        return color;
+    }
+
+    return color;
+  };
+
   return (
-    <span className=" inline-block rounded-full px-2 py-1 text-xs font-semibold text-gray-700 bg-green-200">
-      feito
+    <span
+      className={`inline-block rounded-full px-2 py-1 text-xs font-semibold text-gray-700 ${getBgColor(
+        status
+      )}`}
+    >
+      {status}
     </span>
   );
 };
